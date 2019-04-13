c8f32910462cc5f1339ad00a1409185a78011034
Ann Cerveny <cervann11@gmail.com>
2019-04-13 11:56:36 -0600

    Remove button borders and fix margin

---
+++ todo-list.js
@@ -0,0 +1,24 @@
+class ToDoList {
+  constuctor(id, title, tasks) {
+    this.id = id;
+    this.title = title;
+    this.tasks = task || [];
+    this.urgent = false;
+  }
+
+  saveToStorage() {
+
+  }
+
+  deleteFromStorage() {
+
+  }
+
+  updateToDo() {
+
+  }
+
+  updateTask() {
+    
+  }
+}
