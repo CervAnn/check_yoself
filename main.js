3105e435679a352b84559e040b441017489577b4
Ann Cerveny <cervann11@gmail.com>
2019-04-15 08:20:59 -0600

    Add append card div

--- main.js
+++ main.js
@@ -5,11 +5,14 @@ var addItemButton = document.querySelector(".sidebar__form2--image");
 var addListButton = document.querySelector(".sidebar__form2--button2")
 var clearButton = document.querySelector(".sidebar__form2--button3");
 var filterButton = document.querySelector(".sidebar__form2--button4");
+var cardContainer = document.querySelector(".todo");
 var task = [];
 
 
 // Event Listeners
 window.addEventListener('load', loadPage);
+taskInput.addEventListener('keyup', enableItemButton)
+titleInput.addEventListener('keyup', enableListButton)
 addItemButton.addEventListener('click', createItem);
 // addTaskButton.addEventListener('click', appendItem)
 
@@ -27,26 +30,56 @@ function loadPage() {
 }
 
 function enableButton(e) {
-  if (taskInput.value.length === 0) {
+  if (taskInput.value.length > 0) {
+    addItemButton.disabled = false;
+  } else {
     addItemButton.disabled = true;
-    console.log(true);
+  }
+}
+
+function enableItemButton(e) {
+  if (taskInput.value === "") {
+    addItemButton.disabled = true;
   } else {
     addItemButton.disabled = false;
-    console.log(false);
+  }
+}
+
+function enableListButton(e) {
+  if (titleInput.value === "") {
+    addListButton.disabled = true;
+  } else {
+    addListButton.disabled = false;
   }
 }
 
 function createItem(item) {
-var ul = document.querySelector("#sidebar__list");
-var li = document.createElement('li');
-var deleteItemImage = document.createElement('img');
-deleteItemImage.src = "check-yo-self-icons/delete.svg";
-deleteItemImage.classList.add('sidebar__list--image--delete');
-li.appendChild(deleteItemImage);
-li.appendChild(document.createTextNode(taskInput.value));
-ul.appendChild(li); 
-taskInput.value = "";
-deleteItemImage.onclick = eraseItem;
+  var ul = document.querySelector("#sidebar__list");
+  var li = document.createElement('li');
+  var deleteItemImage = document.createElement('img');
+  deleteItemImage.src = "check-yo-self-icons/delete.svg";
+  deleteItemImage.classList.add('sidebar__list--image--delete');
+  li.appendChild(deleteItemImage);
+  li.appendChild(document.createTextNode(taskInput.value));
+  ul.appendChild(li); 
+  taskInput.value = "";
+  deleteItemImage.onclick = eraseItem;
+}
+
+function createCard(item) {
+  var listCard = `
+  <div class="todo__card--container">
+    <header class="todo__card--header">${titleInput.value}</header>
+    <section class="todo__card--middle">
+      <p class="todo__card--task"></p>
+    </section>
+    <section class="todo__card--footer">
+      <button class="todo__card--button--urgent" src="check-yo-self-icons/urgent.svg"/>
+      <button class="todo__card--button--urgent" src="check-yo-self-icons/urgent-active.svg"/>
+    </section>
+  </div>
+  `
+  cardContainer.insertAdjacentHTML('afterbegin', listCard)
 }
 
 function eraseItem(e) {
