630b97838a199ed0a461e16802bb987a60f72d43
Ann Cerveny <cervann11@gmail.com>
2019-04-15 00:39:26 -0600

    Enable append list and item

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 // Global Variables
 var titleInput = document.querySelector(".navbar__form1--input");
 var taskInput = document.querySelector(".sidebar__form2--input");
-var addTaskButton = document.querySelector(".sidebar__form2--image");
+var addItemButton = document.querySelector(".sidebar__form2--image");
 var addListButton = document.querySelector(".sidebar__form2--button2")
 var clearButton = document.querySelector(".sidebar__form2--button3");
 var filterButton = document.querySelector(".sidebar__form2--button4");
-var hiddenList = document.querySelector(".sidebar__list");
 var task = [];
 
 
 // Event Listeners
+window.addEventListener('load', loadPage);
+addItemButton.addEventListener('click', createItem);
 // addTaskButton.addEventListener('click', appendItem)
 
 // Functions
@@ -20,3 +21,35 @@ var task = [];
 //var add text to created element (innerText
 //add text to the DOM
 
+function loadPage() {
+  addListButton.disabled = true;
+  addItemButton.disabled = true;
+}
+
+function enableButton(e) {
+  if (taskInput.value.length === 0) {
+    addItemButton.disabled = true;
+    console.log(true);
+  } else {
+    addItemButton.disabled = false;
+    console.log(false);
+  }
+}
+
+function createItem(item) {
+var ul = document.querySelector("#sidebar__list");
+var li = document.createElement('li');
+var deleteItemImage = document.createElement('img');
+deleteItemImage.src = "check-yo-self-icons/delete.svg";
+deleteItemImage.classList.add('sidebar__list--image--delete');
+li.appendChild(deleteItemImage);
+li.appendChild(document.createTextNode(taskInput.value));
+ul.appendChild(li); 
+taskInput.value = "";
+deleteItemImage.onclick = eraseItem;
+}
+
+function eraseItem(e) {
+var li = document.createElement('li');
+e.target.parentElement.remove(li);
+}
