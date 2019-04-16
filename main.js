d5ea0781c6889f1b7a52246e1ec00114dfee6a95
Ann Cerveny <cervann11@gmail.com>
2019-04-15 22:35:18 -0600

    Refactoring initial js

--- main.js
+++ main.js
@@ -1,22 +1,91 @@
 // Global Variables
-var titleInput = document.querySelector(".navbar__form1--input");
+var titleInput = document.querySelector(".sidebar__form1--input");
+var ul = document.querySelector("#sidebar__list");
 var taskInput = document.querySelector(".sidebar__form2--input");
 var addItemButton = document.querySelector(".sidebar__form2--image");
-var addListButton = document.querySelector(".sidebar__form2--button2")
+var addListButton = document.querySelector(".sidebar__form2--button2");
 var clearButton = document.querySelector(".sidebar__form2--button3");
 var filterButton = document.querySelector(".sidebar__form2--button4");
 var cardContainer = document.querySelector(".todo");
-var task = [];
-
+var taskCollection = [];
 
 // Event Listeners
 window.addEventListener('load', loadPage);
-taskInput.addEventListener('keyup', enableItemButton)
-titleInput.addEventListener('keyup', enableListButton)
-addItemButton.addEventListener('click', createItem);
-// addTaskButton.addEventListener('click', appendItem)
+taskInput.addEventListener('input', enableItemButton);
+titleInput.addEventListener('input', enableListButton);
+addItemButton.addEventListener('click', createTask);
+addListButton.addEventListener('click', compileTasks);
+ul.addEventListener('click', removeTask);
+
 
 // Functions
+
+function loadPage(e) {
+  addItemButton.disabled = true;
+  addListButton.disable = true;
+}
+
+function enableItemButton(e) {
+  if (taskInput.value !== "") {
+    addItemButton.disable = false;
+    console.log("yay");
+  }
+}
+
+function enableListButton(e) {
+  if (titleInput.value !== "") {
+    addListButton.disable = false;
+        console.log("yipee");
+  }
+}
+
+function createTask(item) {
+  ul.innerHTML += `
+  <li class="sidebar__list--container">
+    <img class="sidebar__list--image--delete" src="check-yo-self-icons/delete.svg">
+    <p class="sidebar__list--item" data-id=${Date.now()}>${taskInput.value}</p>
+  </li>
+  `;
+  taskInput.value = "";
+}
+
+function compileTasks(e) {
+  var taskItems = document.querySelectorAll('.sidebar__list--item');
+  if (titleInput.value && taskItems.innerHTML !== "")
+    var listInstance = new toDoList(titleInput.value, tasks)
+
+
+  var taskItems = document.querySelectorAll('.sidebar__list--item');
+  console.log(taskItems);
+  // for (i = 0; i < taskCollection.length; i++) {
+
+  // }
+}
+
+function createTaskInstance() {
+  var urgent = false;
+  var itemInstance = new ToDoList(Date.now(), titleInput.value, taskInput.value, urgent);
+  tasks.push(itemInstance);
+}
+
+function appendTaskList() {
+  console.log("test2");
+}
+
+function removeTask(e) {
+  e.target.closest('li').remove();
+}
+
+
+
+
+
+
+
+
+
+
+
 
 //create a hidden ul for which we can add an image and text li (item)
 //var createElement
@@ -24,65 +93,95 @@ addItemButton.addEventListener('click', createItem);
 //var add text to created element (innerText
 //add text to the DOM
 
-function loadPage() {
-  addListButton.disabled = true;
-  addItemButton.disabled = true;
-}
+// function restoreCards() {
+//   var taskObject = JSON.parse(localStorage.getItem("item")) || [];
+// }
 
-function enableButton(e) {
-  if (taskInput.value.length > 0) {
-    addItemButton.disabled = false;
-  } else {
-    addItemButton.disabled = true;
-  }
-}
 
-function enableItemButton(e) {
-  if (taskInput.value === "") {
-    addItemButton.disabled = true;
-  } else {
-    addItemButton.disabled = false;
-  }
-}
+/* TESTING */
 
-function enableListButton(e) {
-  if (titleInput.value === "") {
-    addListButton.disabled = true;
-  } else {
-    addListButton.disabled = false;
-  }
-}
+// function loadPage() {
+//   addItemButton.disabled = true;
+//   addListButton.disabled = true;
+// }
 
-function createItem(item) {
-  var ul = document.querySelector("#sidebar__list");
-  var li = document.createElement('li');
-  var deleteItemImage = document.createElement('img');
-  deleteItemImage.src = "check-yo-self-icons/delete.svg";
-  deleteItemImage.classList.add('sidebar__list--image--delete');
-  li.appendChild(deleteItemImage);
-  li.appendChild(document.createTextNode(taskInput.value));
-  ul.appendChild(li); 
-  taskInput.value = "";
-  deleteItemImage.onclick = eraseItem;
-}
+// function enableItemButton(e) {
+//   if (taskInput.value !== "") {
+//     addItemButton.disabled = false;
+//   } 
+// }
 
-function createCard(item) {
-  var listCard = `
-  <div class="todo__card--container">
-    <header class="todo__card--header">${titleInput.value}</header>
-    <section class="todo__card--middle">
-      <p class="todo__card--task"></p>
-    </section>
-    <section class="todo__card--footer">
-      <button class="todo__card--button--urgent" src="check-yo-self-icons/urgent.svg"/>
-      <button class="todo__card--button--urgent" src="check-yo-self-icons/urgent-active.svg"/>
-    </section>
-  </div>
-  `
-  cardContainer.insertAdjacentHTML('afterbegin', listCard)
-}
+// function enableListButton(e) {
 
-function eraseItem(e) {
-var li = document.createElement('li');
-e.target.parentElement.remove(li);
-}
+//   if (titleInput.value !== "") {
+//     addListButton.disabled = false;
+//   }
+// }
+
+
+// function createTask(item) {
+//   var ul = document.querySelector("#sidebar__list");
+//   var li = document.createElement('li');
+//   var deleteItemImage = document.createElement('img');
+//   deleteItemImage.src = "check-yo-self-icons/delete.svg";
+//   deleteItemImage.classList.add('sidebar__list--image--delete');
+//   li.appendChild(deleteItemImage);
+//   li.appendChild(document.createTextNode(taskInput.value));
+//   ul.appendChild(li); 
+//   taskInput.value = "";
+//   deleteItemImage.onclick = eraseItem;
+// }
+
+// function createTaskList(items) {
+//   var urgent = false
+//   var listObject = new ToDoList(Date.now(), titleInput.value, tasks.value, urgent);
+//   console.log(tasks);
+//   // listObject.push(items);
+//   createCard();
+// }
+
+// function createCard() {
+//   console.log(true);
+//   var listCard = `
+//   <div class="todo__card--container">
+//     <article class="todo__card--header">
+//       <h3 class="todo__card--title">${titleInput.value}</h3>
+//     </article>
+//     <section class="todo__card--middle">
+//       <img/>
+//       <p class="todo__card--task">${tasks}</p>
+//     </section>
+//     <article class="todo__card--footer">
+//       <img class="todo__card--button--urgent" src="check-yo-self-icons/urgent.svg"/>
+//       <img class="todo__card--button--delete" src="check-yo-self-icons/delete.svg"/>
+//     </article>
+//   </div>
+//   `;
+//   cardContainer.insertAdjacentHTML('afterbegin', listCard)
+//   titleInput.value = "";
+
+// }
+
+// function eraseItem(e) {
+// e.target.parentElement.remove('li');
+// }
+
+/*
+1. create an object out of the task title and items
+2. push object into an instance with same properties as card
+3. move the object into the appended card
+*/
+
+// function populateCard(titleInput, tasks) {
+//   tasks.forEach(function(item) {
+//     item = 
+//   }
+// }
+
+// function pushTask(task) {
+//   var urgent = false;
+//   var item = new ToDoList(Date.now(), titleInput.value, taskInput.value, urgent);
+//   taskObject.push(item);
+//   item.saveToStorage(taskObject);
+//   createCard(item);
+// }
--- todo-list.js
+++ todo-list.js
@@ -7,6 +7,7 @@ class ToDoList {
   }
 
   saveToStorage() {
+  var stringified = JSON.stringify(taskObject);
 
   }
 
